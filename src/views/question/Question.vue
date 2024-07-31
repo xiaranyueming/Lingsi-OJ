df71b4878cbe4f2f04b58c6f7a86639f91c51c59
zhoushu <2628213214@qq.com>
2024-07-31 11:44:52 +0800

    实现根据一定条件对题目的搜索功能

--- src/views/question/Question.vue
+++ src/views/question/Question.vue
@@ -1,6 +1,10 @@
 <script setup>
 import {onMounted, ref} from "vue";
 import { getQuestionListApi } from "@/apis/question.js";
+import {useUserStore} from "@/stores/user.js";
+import {RoleEnum} from "@/utils/RoleEnum.js";
+
+const userStore = useUserStore()
 
 const columns = [
   {
@@ -24,22 +28,58 @@ const columns = [
     align: 'center'
   },
   {
+    title: '通过数',
+    dataIndex: 'acceptNum',
+    key: 'acceptNum',
+    width: 100,
+    align: 'center'
+  },
+  {
+    title: '提交数',
+    dataIndex: 'submitNum',
+    key: 'submitNum',
+    width: 100,
+    align: 'center'
+  },
+  {
     title: "操作",
     key: "action",
     align: 'center'
   }
 ];
 const data = ref([])
+// 分页信息
 const page = ref({
   pageNum: 1,
   pageSize: 10,
-  keyword: ''
+  keyword: '',
+  questionIndex: null
 })
+// 获取题目数据
 const getQuestionList = async () => {
   const res = await getQuestionListApi(page.value)
   if (res.code === 200) {
     data.value = res.data
   }
+}
+// 重置
+const reset = async () => {
+  page.value = {
+    pageNum: 1,
+    pageSize: 10,
+    keyword: '',
+    questionIndex: null
+  }
+  await getQuestionList()
+}
+// 搜索
+const search = async () => {
+  await getQuestionList()
+}
+
+// 发布题目
+const publishQuestion = () => {
+  
 }
 
 onMounted(() => {
@@ -48,6 +88,13 @@ onMounted(() => {
 </script>
 
 <template>
+  <div class="search">
+    <a-input-number class="inputNumber" v-model:value="page.questionIndex" placeholder="请输入题目序号" :min="1" />
+    <a-input class="keywordInput" v-model:value="page.keyword" placeholder="请输入搜索词" />
+    <a-button ghost class="search-btn" type="primary" @click="search">搜索</a-button>
+    <a-button ghost class="reset-btn" type="primary" @click="reset">重置</a-button>
+    <a-button ghost class="pub-btn" type="primary" @click="publishQuestion" :disabled="userStore.getUser.role !== RoleEnum.ADMIN">发布题目</a-button>
+  </div>
   <a-table :columns="columns" :data-source="data">
     <template #bodyCell="{ column, record }">
       <template v-if="column.key === 'tags'">
@@ -63,5 +110,25 @@ onMounted(() => {
 </template>
 
 <style scoped>
-
+.search {
+  display: flex;
+  align-content: center;
+  margin-bottom: 15px;
+  .inputNumber {
+    width: 150px;
+    margin-right: 30px;
+  }
+  .keywordInput {
+    width: 150px;
+    margin-right: 50px;
+  }
+  .search-btn {
+    margin-right: 30px;
+  }
+  .pub-btn {
+    position: absolute;
+    top: 97px;
+    right: 200px;
+  }
+}
 </style>
